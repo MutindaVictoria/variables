9cfd0877f6650937a40f08ba4942242ad05e3834
MutindaVictoria <victoriaamutinda@gmail.com>
2023-02-02 22:27:53 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Victoria"
+    var age: Byte = 22
+    var phoneNumber: Int = 748914372
+    var weightInkg: Float = 57.9F
+    var citizen: Boolean = true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weightInkg)
+    println(citizen)
+
+}
